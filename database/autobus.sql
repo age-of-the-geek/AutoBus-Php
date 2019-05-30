b3dfdd34237d88ca6a80000395000d952d716c2a
age-of-the-geek <coldstunner786@gmail.com>
2019-05-30 13:29:41 +0500

    db

--- database/autobus.sql
+++ database/autobus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2019 at 08:15 AM
+-- Generation Time: May 30, 2019 at 10:28 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -70,10 +70,13 @@ CREATE TABLE `bus_detail` (
 --
 
 INSERT INTO `bus_detail` (`id`, `bus_number`, `bus_total_seats`, `bus_available_seats`, `bus_image`, `tags`, `bus_route`, `bus_leaving_time`, `bus_reaching_time`, `bus_driver_name`, `bus_ticketchecker_name`, `bus_rating`, `bus_break_time`, `bus_company`, `day`) VALUES
-(7, 'ghj455', '80', '70', 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Leyland_National_Greenway_nearside.jpg', 'Bus', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'asad', 'aaaa', '4', '0000-00-00 00:00:00', 'dewo', 'Monday'),
+(7, 'ghj455', '80', '70', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/London_Transport_RF503.JPG/800px-London_Transport_RF503.JPG', 'Bus', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'asad', 'aaaa', '4', '0000-00-00 00:00:00', 'dewo', 'Monday'),
 (9, '', '', '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/London_Transport_RF503.JPG/800px-London_Transport_RF503.JPG', 'Wfg', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', 'Niazi', 'Thursday'),
 (10, '', '', '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/London_Bus_route_139_A.jpg/800px-London_Bus_route_139_A.jpg', 'Bilal', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', 'Niazi', 'Tuesday'),
-(13, '', '', '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Big_Bus_Company_10-5-07.jpg/800px-Big_Bus_Company_10-5-07.jpg', 'Image', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', 'Niazi', 'Wednesday');
+(13, '', '', '', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Big_Bus_Company_10-5-07.jpg/800px-Big_Bus_Company_10-5-07.jpg', 'Image', 'Lahore to Pindi', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', 'Niazi', 'Wednesday'),
+(14, '', '', '', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', '', ''),
+(15, '', '', '', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', '', ''),
+(16, '', '', '', 'IMG_20170129_223814.jpg', 'Bus', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '', '', '', '0000-00-00 00:00:00', '', '');
 
 -- --------------------------------------------------------
 
@@ -120,6 +123,30 @@ INSERT INTO `subadmin_account` (`id`, `subadmin_uname`, `subadmin_password`, `su
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ticket_details`
+--
+
+CREATE TABLE `ticket_details` (
+  `id` int(11) NOT NULL,
+  `ticket_code` varchar(20) NOT NULL,
+  `ticket_qr` varchar(300) NOT NULL,
+  `passenger_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ticket_details`
+--
+
+INSERT INTO `ticket_details` (`id`, `ticket_code`, `ticket_qr`, `passenger_name`) VALUES
+(14, 'ticket#GbZ56', '1558861468226.png', ''),
+(17, 'ticket#wmZur', '1558864124393.png', ''),
+(18, 'ticket#yH2kJ', '1558864911561.png', ''),
+(19, 'ticket#VxUUI,', '1558868384622.png', ''),
+(20, 'ticket#iur82', '1558901896192.png', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tk_checker_account`
 --
 
@@ -129,6 +156,13 @@ CREATE TABLE `tk_checker_account` (
   `tk_checker_password` text NOT NULL,
   `tk_checker_id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tk_checker_account`
+--
+
+INSERT INTO `tk_checker_account` (`id`, `tk_checker_uname`, `tk_checker_password`, `tk_checker_id`) VALUES
+(1, 'asad@gmail.com', '000000', '');
 
 --
 -- Indexes for dumped tables
@@ -159,6 +193,12 @@ ALTER TABLE `subadmin_account`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tk_checker_account`
 --
 ALTER TABLE `tk_checker_account`
@@ -178,7 +218,7 @@ ALTER TABLE `admin_account`
 -- AUTO_INCREMENT for table `bus_detail`
 --
 ALTER TABLE `bus_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `driver_account`
@@ -193,10 +233,16 @@ ALTER TABLE `subadmin_account`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `ticket_details`
+--
+ALTER TABLE `ticket_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT for table `tk_checker_account`
 --
 ALTER TABLE `tk_checker_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
